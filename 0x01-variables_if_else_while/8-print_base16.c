7ab1a4c8d14777492c0bf5588c76d7b00bfdde3b
Don452 <biny452@gmail.com>
2023-02-19 13:30:05 -0800

    ./8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,19 @@ int main(void)
 {
 	int c;
 
+	int d;
+
 	c = 0;
+	d = 'a';
 	while
 		(c <= 16) {
 			putchar(c + '0');
 			c++;
+		}
+	while
+		(d <= 'f') {
+			putchar(d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
